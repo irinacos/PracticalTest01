ae6a70dfab3ba32ddb90c6e85619f52253950df9
irinacos <irinateocostache@gmail.com>
2024-11-06 15:45:14 +0200

    first commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.kt
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.eim.practicaltest01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PracticalTest01MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_practical_test01_main)
+    }
+}
